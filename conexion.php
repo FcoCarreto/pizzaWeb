eb992bc09433e0cd92fa2adf76ab8e1f1de0634b
FcoCarreto <l181061027@mulege.tecnm.mx>
2021-11-20 18:06:31 -0700

    Se agregaro el archivo de conexion y el de ingresar

---
+++ conexion.php
@@ -0,0 +1,11 @@
+<?php
+$host = "localhost";
+$usuario = "root";
+$contraseña = "";
+$base = "bdpizza"; //BD creada por nosotros//
+$conexion = new mysqli($host,$usuario,$contraseña,$base);
+if ($conexion -> connect_errno)
+	{
+die("Fallo la conexion :(".$conexion -> mysqli_connect_errno().")".$conexion -> mysqli_connect_error());
+	}
+?>
